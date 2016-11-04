280999f159293dbbd9174c252b50ef8ed28a010f
Patricio José <soriwert@gmail.com>
2016-11-04 18:57:41 +0100

    para poder guardar

---
+++ Ejercicio02/src/ejercicio02.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ejercicio02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner (System.in);
+		
+		int n=1;
+		int m=1;
+		int r=1;
+		
+		
+		System.out.println ("Introduzca el número que desea multiplicar: ");
+		n=teclado.nextInt();
+		/*while (n >= 1 && n<=100){
+			System.out.println("Tabla de multiplicar de "+n);*/
+			switch (n){
+			case 1: n * 1 = m;
+			}
+			
+		}
+	}
+		
+			
+		
+
+
