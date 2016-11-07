c29d67c6ad7d56c02b7c6057abba96f51c482576
Patricio José <soriwert@gmail.com>
2016-11-07 19:24:55 +0100

    07/11/2016

--- Ejercicio04/src/ejercicio04.java
+++ Ejercicio04/src/ejercicio04.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
-public class Ejercicio03 {
+public class ejercicio04 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner (System.in);
 
-
 		int n,a;
-		
-		
 
-
-		System.out.println("Escriba un número n ");
+		System.out.println("Escriba un número n: ");
 		n=teclado.nextInt();
 		a=n-1;
-		System.out.print("Los divisores son: ");
-		while (a!=0){
 
-		
-		if (n%a==0){
-			System.out.print(a+" ");
+		while (a>1){
+			if (n%a==0){
 
+				System.out.println("");
+				a=1;
+			}
+			--a;
 		}
-		--a;
+		System.out.println (n+" Es primo ");
 	}
+	
+
 }
-}
+
+
