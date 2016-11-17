b39eb2547cbf8c6fb6c08225cb28e3c881900e9e
Patricio José <soriwert@gmail.com>
2016-11-17 19:25:32 +0100

    17/11/2016

--- ejercicio10/src/ejercicio10.java
+++ ejercicio10/src/ejercicio10.java
@@ -1,13 +1,43 @@
-import util.java.Scanner;
+import java.util.Scanner;
 public class ejercicio10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner (System.in);
-		
-		int n; a;
-		a=n-1;
 
+		int n, divisor=2,p=1;
+		boolean esPrimo=true;
+
+		System.out.println("Introduzca un numero N: ");
+		n=teclado.nextInt();
+		p=n;
+		do {
+			p++;
+		
+		    // compruebo si p es primo
+			
+			
+		} while (!esPrimo);
+			esPrimo=p%divisor !=0;
+			divisor++;
+		
+		
+		System.out.println("El número "+p+" es");
+		System.out.println ((esPrimo ? "" : " no ") + "primo");
+		
+		
+		
+		}
 	}
 
-}//http://lineadecodigo.com/java/numeros-primos-en-java/
+/*while (esPrimo && divisor < n){
+esPrimo=n%divisor !=0;
+divisor++;
+
+while (esPrimo && divisor > n)
+	n++;
+	esPrimo=n%divisor !=0;
+}*/
+
+
+
